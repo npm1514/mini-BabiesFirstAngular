d0b5d5f367b2840b41982679dc573ff3f5f819c2
Nick Marucci <npm1514@gmail.com>
2015-12-14 09:43:39 -0700

    Fixed the bug.

--- js/mainCtrl.js
+++ js/mainCtrl.js
@@ -1,8 +1,9 @@
 var app = angular.module("friendsList");
 app.controller("mainCtrl", function($scope){
-	$scope.friends = {name: "Nick"};
+	$scope.friends = ["Nick"];
+
 	$scope.addFriend = function() {
-		$scope.friends.push(user);
+		$scope.friends.push($scope.newFriend);
 	};
 
 });
